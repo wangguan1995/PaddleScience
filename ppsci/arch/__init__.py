b8aba3cfd411a73f8e9beb20e48bf516f4486923
Lil_Ken <98253413+LilaKen@users.noreply.github.com>
2025-01-06 22:22:59 +0800

    Update __init__.py

--- ppsci/arch/__init__.py
+++ ppsci/arch/__init__.py
@@ -17,13 +17,6 @@ from __future__ import annotations
 import copy
 
 from ppsci.arch.regdgcnn import RegDGCNN
-from ppsci.arch.regpointnet import RegPointNet
-from ppsci.arch.transolver import Transolver_Airfoil
-from ppsci.arch.transolver import Transolver_Irregular_Mesh_Elas
-from ppsci.arch.transolver import Transolver_ShapeNetCar
-from ppsci.arch.transolver import Transolver_Structured_Mesh_2D
-from ppsci.arch.transolver import Transolver_Structured_Mesh_3D
-
 from ppsci.arch.afno import AFNONet  # isort:skip
 from ppsci.arch.afno import PrecipNet  # isort:skip
 from ppsci.arch.amgnet import AMGNet  # isort:skip
@@ -111,13 +104,7 @@ __all__ = [
     "USCNN",
     "VelocityDiscriminator",
     "VelocityGenerator",
-    "Transolver_ShapeNetCar",
-    "Transolver_Structured_Mesh_3D",
-    "Transolver_Structured_Mesh_2D",
-    "Transolver_Irregular_Mesh_Elas",
-    "Transolver_Airfoil",
     "RegDGCNN",
-    "RegPointNet",
 ]
 
 
